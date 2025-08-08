0fe4d36943e4cb5cb1aa244d1cf3f4c9ef39faea
SnowCharm <qiuyilun@u.nus.edu>
2025-08-08 22:08:37 +0800

    update evaluation

--- model-eval.py
+++ model-eval.py
@@ -1,18 +1,21 @@
 import os
 import sys
-import json
 import torch
 import argparse
 import evaluate
 import warnings
+import numpy as np
 import torch.distributed as dist
 
-from transformers import set_seed
 from datasets import load_from_disk
+from vllm import LLM, SamplingParams
+from bert_score import score as bert_score
+from transformers import set_seed, logging
 
 from utils.utils import postprocess_output
 
 warnings.filterwarnings("ignore")
+logging.set_verbosity_error()
 
 set_seed(42)
 
@@ -24,33 +27,35 @@ parser.add_argument("--category", required=True,
 parser.add_argument("--eval", required=True, choices=["eval", "infer"])
 parser.add_argument("--temperature", type=float, default=0.8)
 parser.add_argument("--his_len", type=int, default=8)
+parser.add_argument("--gpu", default="0")
 
 args = parser.parse_args()
 category = args.category
 
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 
 if __name__ == "__main__":
 
-    import vllm
-
     if args.eval == "infer":
-        with open("results.json", "r", encoding="utf-8") as f:
-            all_results = json.load(f)
-        best_model = max(all_results, key=lambda x: x['meteor'])
-        model_name = best_model['model']
-        sampling_params = vllm.SamplingParams(
+        # with open("results.json", "r", encoding="utf-8") as f:
+        #     all_results = json.load(f)
+        # best_model = max(all_results, key=lambda x: x['meteor'])
+        # model_name = best_model['model']
+        model_name = "SnowCharmQ/DEP-model"
+        sampling_params = SamplingParams(
             max_tokens=2048,
             skip_special_tokens=True,
             temperature=args.temperature,
             top_p=0.95
         )
-        llm = vllm.LLM(
+        llm = LLM(
             model_name,
+            dtype="bfloat16",
             gpu_memory_utilization=0.9,
             enforce_eager=True
         )
 
-        personal_dataset = load_from_disk(f"data/dataset_test_{category}_{args.his_len}")
+        personal_dataset = load_from_disk(f"data/dataset_test_{category}")
         prompts = personal_dataset['inp_str']
         his_diff_embs = personal_dataset['his_diff_emb']
         his_diff_embs = [torch.tensor(his_diff_emb)
@@ -64,13 +69,14 @@ if __name__ == "__main__":
                     for pred in predictions]
         if not os.path.exists("output"):
             os.makedirs("output")
-        with open(f"output/predictions_{category}_{args.his_len}.txt", "w", encoding="utf-8") as f:
+        with open(f"output/predictions_{category}.txt", "w", encoding="utf-8") as f:
             for pred in predictions:
                 f.write(pred + "\n---------------------------------\n")
     elif args.eval == "eval":
-        personal_dataset = load_from_disk(f"data/dataset_test_{category}_{args.his_len}")
+        personal_dataset = load_from_disk(f"data/dataset_test_{category}")
         references = personal_dataset['out_str']
-        predictions_path = f"output/predictions_{category}_{args.his_len}.txt"
+        references = list(references)
+        predictions_path = f"output/predictions_{category}.txt"
         with open(predictions_path, 'r', encoding='utf-8') as f:
             predictions = f.read()
             predictions = predictions.split('\n---------------------------------\n')
@@ -79,17 +85,31 @@ if __name__ == "__main__":
         bleu_metric = evaluate.load("sacrebleu")
         rouge_metric = evaluate.load('rouge')
         meteor_metric = evaluate.load('meteor')
-        result_bleu = bleu_metric.compute(predictions=predictions,
-                                        references=references)
-        result_rouge = rouge_metric.compute(predictions=predictions,
-                                            references=references)
-        result_meteor = meteor_metric.compute(predictions=predictions,
-                                            references=references)
+        result_bleu = bleu_metric.compute(
+            predictions=predictions,
+            references=references
+        )
+        result_rouge = rouge_metric.compute(
+            predictions=predictions,
+            references=references
+        )
+        result_meteor = meteor_metric.compute(
+            predictions=predictions,
+            references=references
+        )
+        P, R, F1 = bert_score(
+            predictions, 
+            references, 
+            model_type="allenai/led-base-16384",
+            lang="en", 
+            verbose=False,
+        )
         result = {
-            "rouge-1": result_rouge["rouge1"],
-            "rouge-L": result_rouge["rougeL"],
-            "meteor": result_meteor['meteor'],
-            "bleu": result_bleu["score"],
+            "rouge-1": result_rouge["rouge1"].item() if isinstance(result_rouge["rouge1"], np.float64) else result_rouge["rouge1"],
+            "rouge-L": result_rouge["rougeL"].item() if isinstance(result_rouge["rougeL"], np.float64) else result_rouge["rougeL"],
+            "meteor": result_meteor['meteor'].item() if isinstance(result_meteor['meteor'], np.float64) else result_meteor['meteor'],
+            "bleu": result_bleu["score"].item() if isinstance(result_bleu["score"], np.float64) else result_bleu["score"],
+            "bertscore": F1.mean().item(),
         }
         print(result)
 
--- run-eval.sh
+++ run-eval.sh
@@ -1,10 +1,3 @@
-python -u model-select.py --version 0 --select infer
-python -u model-select.py --version 1 --select infer
-python -u model-select.py --version 2 --select infer
-python -u model-select.py --version 3 --select infer
-python -u model-select.py --version 4 --select infer
-python -u model-select.py --select eval
-
 python -u model-eval.py --category Books --eval infer --temperature 0.8 --dataset test
 python -u model-eval.py --category Books --eval eval --temperature 0.8 --dataset test
 
